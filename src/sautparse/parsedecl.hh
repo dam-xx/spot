5254b3a4b9b534f4c81429afc4ef0f3380d07baa
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2006-03-01 09:53:22 +0000

    src/sautparse/parsedecl.hh: add missing file

---
+++ src/sautparse/parsedecl.hh
@@ -0,0 +1,53 @@
+// Copyright (C) 2006  Laboratoire d'Informatique de Paris 6 (LIP6),
+// département Systèmes Répartis Coopératifs (SRC), Université Pierre
+// et Marie Curie.
+//
+// This file is part of Spot, a model checking library.
+//
+// Spot is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// Spot is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+// License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Spot; see the file COPYING.  If not, write to the Free
+// Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+// 02111-1307, USA.
+
+#ifndef SPOT_SAUTPARSE_PARSEDECL_HH
+# define SPOT_SAUTPARSE_PARSEDECL_HH
+
+#include <string>
+#include "sautparse.hh"
+#include "location.hh"
+
+# define YY_DECL \
+  int sautyylex (yystype *yylval, yy::location *yylloc)
+YY_DECL;
+
+namespace spot
+{
+  int sautyyopen(const std::string& name);
+  void sautyyclose();
+}
+
+
+// Gross kludge to compile yy::Parser in another namespace (sautyy::)
+// but still use yy::Location.  The reason is that Bison's C++
+// skeleton does not support anything close to %name-prefix at the
+// moment.  All parser are named yy::Parser which makes it somewhat
+// difficult to define multiple parsers.
+namespace sautyy
+{
+  using namespace yy;
+}
+#define yy sautyy
+
+
+
+#endif // SPOT_SAUTPARSE_PARSEDECL_HH
