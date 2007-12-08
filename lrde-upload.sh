8d1b5e40b038a738f0344ef2a2454458f3b6798c
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2007-12-08 17:01:47 +0000

    Ahem...  Pass the -R option to the right chmod.

--- lrde-upload.sh
+++ lrde-upload.sh
@@ -8,8 +8,8 @@ set -x
 
 rm -rf /lrde/dload/spot/spot-snapshot.tmp
 cp -pR doc/spot.html /lrde/dload/spot/spot-snapshot.tmp
-chmod a+rX /lrde/dload/spot/spot-snapshot.tmp
+chmod -R a+rX /lrde/dload/spot/spot-snapshot.tmp
 mv -f spot-*.tar.gz /lrde/dload/spot/spot-snapshot.tar.gz
-chmod -R a+rX /lrde/dload/spot/spot-snapshot.tar.gz
+chmod a+rX /lrde/dload/spot/spot-snapshot.tar.gz
 rm -rf /lrde/dload/spot/spot-snapshot.html
 mv -f /lrde/dload/spot/spot-snapshot.tmp /lrde/dload/spot/spot-snapshot.html
