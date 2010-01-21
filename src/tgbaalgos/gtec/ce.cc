79cb3ff512a46938c46ac43cc9951eba0493dad4
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2010-01-21 11:57:44 +0100

    Fix a memory leak in Cou99 statistics.
    * src/tgbaalgos/gtec/ce.cc (couvreur99_check_result::acss_states):
    Delete the iterator after using it.
    * src/tgbatest/emptchkr.test: Run 'randtgba -z' with valgrind too.

--- src/tgbaalgos/gtec/ce.cc
+++ src/tgbaalgos/gtec/ce.cc
@@ -1,6 +1,8 @@
 // Copyright (C) 2004, 2005  Laboratoire d'Informatique de Paris 6 (LIP6),
 // département Systèmes Répartis Coopératifs (SRC), Université Pierre
 // et Marie Curie.
+// Copyright (C) 2010  Laboratoire de Recherche et Développement de
+// l'Epita (LRDE).
 //
 // This file is part of Spot, a model checking library.
 //
@@ -92,6 +94,7 @@ namespace spot
     for (i->first(); !i->done(); i->next())
       if (i->get_index() >= scc_root)
 	++count;
+    delete i;
     return count;
   }
 
