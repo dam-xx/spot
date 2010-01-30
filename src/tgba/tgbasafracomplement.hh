7cc2776d910eafbaec03c7e9d100788e50073563
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2010-01-30 12:02:35 +0100

    * src/tgba/tgbasafracomplement.hh: Add missing copyright and fix some comments.

--- src/tgba/tgbasafracomplement.hh
+++ src/tgba/tgbasafracomplement.hh
@@ -1,3 +1,23 @@
+// Copyright (C) 2009, 2010 Laboratoire de Recherche et Développement
+// de l'Epita (LRDE).
+//
+// This file is part of Spot, a model checking library.
+//
+// Spot is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// Spot is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+// License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Spot; see the file COPYING.  If not, write to the Free
+// Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+// 02111-1307, USA.
+
 #ifndef SPOT_TGBA_TGBASAFRACOMPLEMENT_HH
 # define SPOT_TGBA_TGBASAFRACOMPLEMENT_HH
 
@@ -24,12 +44,12 @@ namespace spot
   /// 2. Interpreting this deterministic Rabin automaton as a
   ///    deterministic Streett will produce a complemented automaton.
   /// 3. Then we use a transformation from deterministic Streett
-  ///    automaton to nondeterministic Büchi automaton.
+  ///    automaton to nondeterministic B�chi automaton.
   ///
   ///  Safra construction is done in \a tgba_complement, the transformation
   ///  is done on-the-fly when successors are called.
   ///
-  /// \sa safra_determinisation, tgba_complement::succ_iter.
+  /// \sa safra_determinisation, tgba_safra_complement::succ_iter.
   class tgba_safra_complement : public tgba
   {
   public:
@@ -74,7 +94,7 @@ namespace spot
   /// \brief Produce a dot output of the Safra automaton associated
   /// to \a a.
   ///
-  /// @param a The \c tgba_complement with an intermediate Safra
+  /// @param a The \c tgba_safra_complement with an intermediate Safra
   /// automaton to display
   ///
   void display_safra(const tgba_safra_complement* a);
